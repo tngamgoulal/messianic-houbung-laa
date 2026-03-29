610f08fc3067a56668c821bb553a4a3d8bd7b0b8
tngamgoulal <tngamgoulal@gmail.com>
2026-03-29 16:38:35 +0530

    Create data/songs.ts file with 12 songs data

---
+++ data/songs.ts
@@ -0,0 +1,159 @@
+const songs = [
+    {
+        id: 1,
+        title: 'MALAIYA ELOHIM',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for MALAIYA ELOHIM, line 1',
+                'Verse 2: Lyric for MALAIYA ELOHIM, line 2',
+                'Verse 3: Lyric for MALAIYA ELOHIM, line 3',
+                'Verse 4: Lyric for MALAIYA ELOHIM, line 4',
+            ],
+            chorus: 'Chorus: Refrain for MALAIYA ELOHIM',
+        }
+    },
+    {
+        id: 2,
+        title: 'THULHUNG JAL A',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for THULHUNG JAL A, line 1',
+                'Verse 2: Lyric for THULHUNG JAL A, line 2',
+                'Verse 3: Lyric for THULHUNG JAL A, line 3',
+            ],
+            chorus: 'Chorus: Refrain for THULHUNG JAL A',
+        }
+    },
+    {
+        id: 3,
+        title: 'KA LHAGAO KINEPNA',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for KA LHAGAO KINEPNA, line 1',
+                'Verse 2: Lyric for KA LHAGAO KINEPNA, line 2',
+                'Verse 3: Lyric for KA LHAGAO KINEPNA, line 3',
+            ],
+            chorus: 'Chorus: Refrain for KA LHAGAO KINEPNA',
+        }
+    },
+    {
+        id: 4,
+        title: 'LHAMBUH KUT HI',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for LHAMBUH KUT HI, line 1',
+                'Verse 2: Lyric for LHAMBUH KUT HI, line 2',
+                'Verse 3: Lyric for LHAMBUH KUT HI, line 3',
+                'Verse 4: Lyric for LHAMBUH KUT HI, line 4',
+            ],
+            chorus: 'Chorus: Refrain for LHAMBUH KUT HI',
+        }
+    },
+    {
+        id: 5,
+        title: 'HUNGUN GEL UN',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for HUNGUN GEL UN, line 1',
+                'Verse 2: Lyric for HUNGUN GEL UN, line 2',
+                'Verse 3: Lyric for HUNGUN GEL UN, line 3',
+            ],
+            chorus: 'Chorus: Refrain for HUNGUN GEL UN',
+        }
+    },
+    {
+        id: 6,
+        title: 'LEISET LHEPNAN',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for LEISET LHEPNAN, line 1',
+                'Verse 2: Lyric for LEISET LHEPNAN, line 2',
+                'Verse 3: Lyric for LEISET LHEPNAN, line 3',
+                'Verse 4: Lyric for LEISET LHEPNAN, line 4',
+            ],
+            chorus: 'Chorus: Refrain for LEISET LHEPNAN',
+        }
+    },
+    {
+        id: 7,
+        title: 'VO ISRAEL',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for VO ISRAEL, line 1',
+                'Verse 2: Lyric for VO ISRAEL, line 2',
+                'Verse 3: Lyric for VO ISRAEL, line 3',
+            ],
+            chorus: 'Chorus: Refrain for VO ISRAEL',
+        }
+    },
+    {
+        id: 8,
+        title: 'VO IJAKAI',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for VO IJAKAI, line 1',
+                'Verse 2: Lyric for VO IJAKAI, line 2',
+                'Verse 3: Lyric for VO IJAKAI, line 3',
+                'Verse 4: Lyric for VO IJAKAI, line 4',
+                'Verse 5: Lyric for VO IJAKAI, line 5',
+                'Verse 6: Lyric for VO IJAKAI, line 6',
+            ],
+            chorus: 'Chorus: Refrain for VO IJAKAI',
+        }
+    },
+    {
+        id: 9,
+        title: 'ELOAH NANGBOU',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for ELOAH NANGBOU, line 1',
+                'Verse 2: Lyric for ELOAH NANGBOU, line 2',
+                'Verse 3: Lyric for ELOAH NANGBOU, line 3',
+                'Verse 4: Lyric for ELOAH NANGBOU, line 4',
+            ],
+            chorus: 'Chorus: Refrain for ELOAH NANGBOU',
+        }
+    },
+    {
+        id: 10,
+        title: 'MESSIAH LENGGAM',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for MESSIAH LENGGAM, line 1',
+                'Verse 2: Lyric for MESSIAH LENGGAM, line 2',
+                'Verse 3: Lyric for MESSIAH LENGGAM, line 3',
+                'Verse 4: Lyric for MESSIAH LENGGAM, line 4',
+                'Verse 5: Lyric for MESSIAH LENGGAM, line 5',
+            ],
+            chorus: 'Chorus: Refrain for MESSIAH LENGGAM',
+        }
+    },
+    {
+        id: 11,
+        title: 'KUMSANG MESSIAH',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for KUMSANG MESSIAH, line 1',
+                'Verse 2: Lyric for KUMSANG MESSIAH, line 2',
+                'Verse 3: Lyric for KUMSANG MESSIAH, line 3',
+                'Verse 4: Lyric for KUMSANG MESSIAH, line 4',
+            ],
+            chorus: 'Chorus: Refrain for KUMSANG MESSIAH',
+        }
+    },
+    {
+        id: 12,
+        title: 'U LE NAO SOPI',
+        sections: {
+            verses: [
+                'Verse 1: Lyric for U LE NAO SOPI, line 1',
+                'Verse 2: Lyric for U LE NAO SOPI, line 2',
+                'Verse 3: Lyric for U LE NAO SOPI, line 3',
+                'Verse 4: Lyric for U LE NAO SOPI, line 4',
+            ],
+            chorus: 'Chorus: Refrain for U LE NAO SOPI',
+        }
+    },
+];
+
+export default songs;
